71dd73ef0da90fae043c158f304f46226a65455d
ebraheemijaz <ebraheemijaz913@gmail.com>
2022-03-15 23:53:02 +0500

    onlineDB

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mongo = require('mongoose');
-const URI = "mongodb://localhost:27017/portal?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const URI = "mongodb+srv://jhoncena:Test%4012345@studentportal.fccdh.mongodb.net/Portal?retryWrites=true&w=majority"
 
 const connecttoDB = () => {
     mongo.connect(URI, (err, db) => {
         if (err) {
             console.log("Database not Connected")
+            console.log(err)
             return;
         }
         console.log(`Connected to database ${db.name}`)
